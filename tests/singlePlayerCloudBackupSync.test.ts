985c1566de2c7934be1ea7d281e67a6de6101681
shawn <me@bentsignal.com>
2026-08-01 08:24:25 -0400

    Resolve cloud backup integration tests

--- tests/singlePlayerCloudBackupSync.test.ts
+++ tests/singlePlayerCloudBackupSync.test.ts
@@ -46,11 +46,12 @@ for (const descriptor of [[1, "world-a", "0", "2", "10"], [2, "0"], [3, "World A
 for (const wire of [[1, 10, [], []], [2, 10], [3, 10, "4"]]) assert.deepEqual(parseSinglePlayerCloudQueryWire(wire), wire);
 for (const wire of [[1, 10, []], [1, 10, [], [], 0], [4, 10], [2, "10"], [1, 8_640_000_000_000_001, [], []],
   [3, 10], [3, 10, "bad"], [1, 10, new Array(1), []], {}, null]) assert.equal(parseSinglePlayerCloudQueryWire(wire), null);
-const mutationWires = [[1, "1", 10], [2, "2", 10], [3, "cloud_capacity", 10], [3, "world_limit", 10],
+const mutationWires = [[1, "1", 10], [2, "0", 10], [2, "2", 10], [3, "cloud_capacity", 10], [3, "world_limit", 10],
   [4, 10], [5, "conflict", 10], [6, 1, 10], [7, "3", 0, 10], [7, "3", 1, 10], [8, "4", 10]];
 for (const wire of mutationWires) assert.deepEqual(parseSinglePlayerCloudMutationWire(wire), wire);
 const sparse = new Array(3); sparse[0] = 1; sparse[2] = 10;
-for (const wire of [[1, "1"], [1, "1", 10, 0], [2, 10], [7, "conflict", 10], [7, "0", 1, 10], [7, "2", 2, 10],
+for (const wire of [[1, "1"], [1, "1", 10, 0], [1, "0", 10], [8, "0", 10], [2, 10],
+  [7, "conflict", 10], [7, "0", 1, 10], [7, "2", 2, 10],
   [7, "2", 1, 8_640_000_000_000_001],
   [1, String(Number.MAX_SAFE_INTEGER), 10], [3, "conflict", 10], [5, "", 10], [6, 0, 10],
   [6, 8_640_000_000_000_001, 10], [2, 8_640_000_000_000_001], sparse, {}, null]) {
